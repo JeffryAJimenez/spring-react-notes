5aa1274aed48352daf4884a37c3b35ad1252459b
JeffryAJimenez <jeffry.jimenez@outlook.com>
2022-11-14 11:25:17 -0500

    server -> gateway updated to include orders service

--- AuthorizationService/src/main/java/com/jeffryjimenez/AuthorizationService/AuthorizationServiceApplication.java
+++ AuthorizationService/src/main/java/com/jeffryjimenez/AuthorizationService/AuthorizationServiceApplication.java
@@ -2,6 +2,7 @@ package com.jeffryjimenez.AuthorizationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class AuthorizationServiceApplication {
